4a1aebd3a02d2fbddecdd4115553d7e9d6f489d3
unknown <agujeremiah97@gmail.com>
2023-12-01 03:14:04 -0800

    file and folder structure done

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:imisi/main.dart';
+import 'package:imisi/app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
