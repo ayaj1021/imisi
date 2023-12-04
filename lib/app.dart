2b0382e890c5851f20fcc153f6e32fef1327e463
unknown <ayaj1021.com>
2023-12-04 09:21:40 +0100

    onboarding edit

--- lib/app.dart
+++ lib/app.dart
@@ -12,14 +12,11 @@ class MyApp extends StatelessWidget {
     return ScaledSize(
         allowTextScaling: true,
         builder: () {
-          return MaterialApp(
+          return const MaterialApp(
             debugShowCheckedModeBanner: false,
-            title: 'Flutter Demo',
-            theme: ThemeData(
-              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-              useMaterial3: true,
-            ),
-            home: const SplashScreen(),
+            
+         
+            home: SplashScreen(),
           );
         });
   }
