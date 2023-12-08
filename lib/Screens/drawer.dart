e5b530d95103c65a201170e46b9d71237c6549eb
unknown <ayaj1021.com>
2023-12-08 14:11:33 +0100

    automatic login

---
+++ lib/Screens/drawer.dart
@@ -0,0 +1,103 @@
+import 'package:flutter/material.dart';
+import 'package:imisi/Styles/app_colors.dart';
+import 'package:imisi/Styles/app_text_styles.dart';
+import 'package:imisi/Utils/gap.dart';
+import 'package:scaled_size/scaled_size.dart';
+
+class DrawerWidget extends StatelessWidget {
+  const DrawerWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      width: 225.rw,
+      shadowColor: Colors.black87,
+      elevation: 10,
+      backgroundColor: AppColors.overlayColor,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 23),
+        child: ListView(
+          children: [
+            Center(
+              child: SizedBox(
+                height: 60,
+                width: 40,
+                child: Image.asset(
+                  'assets/logos/Imisi_logo1.png',
+                ),
+              ),
+            ),
+            gapHeight(33),
+
+            // CircleAvatar(
+            //   radius: 50,
+            //   child: Image.asset('assets/images/drawer_image.png'),
+            // ),
+            // gapHeight(28),
+
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 28),
+              leading: Image.asset(
+                'assets/images/badge_icon.png',
+                scale: 3,
+              ),
+              title: Text(
+                'Points',
+                style: AppStyles.agTitle3Bold
+                    .copyWith(color: AppColors.onPrimaryColor),
+              ),
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 28),
+              leading: const Icon(
+                Icons.queue_music_outlined,
+                color: AppColors.onPrimaryColor,
+              ),
+              title: Text(
+                'Playlists',
+                style: AppStyles.agTitle3Bold
+                    .copyWith(color: AppColors.onPrimaryColor),
+              ),
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 28),
+              leading: const Icon(
+                Icons.favorite_outline,
+                color: AppColors.onPrimaryColor,
+              ),
+              title: Text(
+                'Favorites',
+                style: AppStyles.agTitle3Bold
+                    .copyWith(color: AppColors.onPrimaryColor),
+              ),
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 28),
+              leading: const Icon(
+                Icons.person_outline,
+                color: AppColors.onPrimaryColor,
+              ),
+              title: Text(
+                'Profile',
+                style: AppStyles.agTitle3Bold
+                    .copyWith(color: AppColors.onPrimaryColor),
+              ),
+            ),
+            ListTile(
+              contentPadding: const EdgeInsets.symmetric(horizontal: 28),
+              leading: const Icon(
+                Icons.logout_outlined,
+                color: AppColors.onPrimaryColor,
+              ),
+              title: Text(
+                'Logout',
+                style: AppStyles.agTitle3Bold
+                    .copyWith(color: AppColors.onPrimaryColor),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/onboard/Onboard Screens/Base/Basepages/home_page.dart
+++ lib/onboard/Onboard Screens/Base/Basepages/home_page.dart
@@ -1,13 +1,12 @@
+// ignore_for_file: avoid_print
+
 import 'dart:convert';
-
 import 'package:flutter/material.dart';
-
-import 'package:imisi/Models/get_all_music_model.dart';
+import 'package:imisi/Screens/drawer.dart';
 import 'package:imisi/Styles/app_colors.dart';
 import 'package:imisi/Styles/app_text_styles.dart';
 import 'package:imisi/Utils/gap.dart';
 import 'package:imisi/Widget/top_artist_widget.dart';
-
 import 'package:http/http.dart' as http;
 import 'package:scaled_size/scaled_size.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -23,139 +22,130 @@ class _HomePageState extends State<HomePage> {
   Future getMusic() async {
     SharedPreferences pref = await SharedPreferences.getInstance();
     String? token = pref.getString("token");
-
     String url = 'https://imisi-backend-service.onrender.com/api/musics';
     var response = await http.get(Uri.parse(url), headers: {
       'Content-Type': 'application/json',
       'Authorization': "Bearer $token",
     });
     var body = jsonDecode(response.body);
-print(body);
+    print(body);
     return body;
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColors.secondaryColor,
+      ),
+      drawer: const DrawerWidget(),
       backgroundColor: AppColors.secondaryColor,
       body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0, top: 20),
-                child: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.menu,
-                  ),
-                  color: Colors.white,
-                ),
-              ),
-              gapHeight(40),
-              Padding(
-                padding: const EdgeInsets.only(left: 15.0),
-                child: Row(
-                  children: [
-                    Text(
-                      "Top Songs",
-                      style: AppStyles.agTitle3Bold.copyWith(
-                        color: Colors.white,
-                      ),
-                    ),
-                    const Spacer(),
-                    //  Image.asset("assets/images/point.png"),
-                    gapWidth(5),
-                    Text(
-                      "8 Points",
-                      style: AppStyles.bodyBold.copyWith(color: Colors.white),
-                    ),
-                    gapWidth(15)
-                  ],
-                ),
-              ),
-              gapHeight(20),
-
-              FutureBuilder(
-                  future: getMusic(),
-                  builder: (context, snapshot) {
-                    return SizedBox(
-                      height: 200,
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: snapshot.data.length,
-                            shrinkWrap: true,
-                        itemBuilder: (context, index) {
-                          return SizedBox(
-                            height: 150,
-                            child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                               Container(
-                                        height: 130.rw,
-                                        width: 110.rh,
-                                        decoration: BoxDecoration(
-                                          color: Colors.red,
-                                          // image: DecorationImage(
-                                          //   fit: BoxFit.fill,
-                                          //   image: NetworkImage(snapshot
-                                          //       .data![index].image!.filePath
-                                          //       .toString()),
-                                          // ),
-                                          borderRadius: BorderRadius.circular(8),
-                                        ),
-                                      ),
-                                gapHeight(5),
-                                Text(snapshot.data![index]["artist"] ?? "Davido",
-                                    style: AppStyles.bodyBold.copyWith(
-                                        color: AppColors.onPrimaryColor)),
-                                gapHeight(2),
-                                Text(snapshot.data![index]["name"],
-                                    style: AppStyles.bodyRegularText.copyWith(
-                                        color: AppColors.onPrimaryColor))
-                              ],
-                            ),
-                          );
-                        },
-                      ),
-                    );
-                  }),
-              ElevatedButton(
-                  onPressed: () {
-                    getMusic();
-                  },
-                  child: Text("Press")),
-              gapHeight(20),
-              Padding(
-                padding: const EdgeInsets.only(left: 15),
-                child: Text(
-                  "Top Artistes",
-                  style: AppStyles.agTitle3Bold.copyWith(
-                    color: Colors.white,
-                  ),
-                ),
-              ),
-              gapHeight(20),
-              const Padding(
-                padding: EdgeInsets.only(left: 15),
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
+          child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 15.0),
                   child: Row(
                     children: [
-                      TopArtisteWidget(),
-                      TopArtisteWidget(),
-                      TopArtisteWidget(),
+                      Text(
+                        "Top Songs",
+                        style: AppStyles.agTitle3Bold.copyWith(
+                          color: Colors.white,
+                        ),
+                      ),
+                      const Spacer(),
+                      //  Image.asset("assets/images/point.png"),
+                      gapWidth(5),
+                      Text(
+                        "8 Points",
+                        style: AppStyles.bodyBold.copyWith(color: Colors.white),
+                      ),
+                      gapWidth(15)
                     ],
                   ),
                 ),
-              )
-            ],
-          ),
-        ],
+                gapHeight(20),
+                FutureBuilder(
+                    future: getMusic(),
+                    builder: (context, snapshot) {
+                      return SizedBox(
+                        height: 200,
+                        child: ListView.builder(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: snapshot.data.length,
+                          shrinkWrap: true,
+                          itemBuilder: (context, index) {
+                            return SizedBox(
+                              height: 250,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Container(
+                                    margin: const EdgeInsets.all(10),
+                                    height: 130.rw,
+                                    width: 110.rh,
+                                    decoration: BoxDecoration(
+                                      color: Colors.red,
+                                      // image: DecorationImage(
+                                      //   fit: BoxFit.fill,
+                                      //   image: NetworkImage(snapshot
+                                      //       .data![index].image!.filePath
+                                      //       .toString()),
+                                      // ),
+                                      borderRadius: BorderRadius.circular(8),
+                                    ),
+                                  ),
+                                  gapHeight(5),
+                                  Text(
+                                      snapshot.data![index]["artist"] ??
+                                          "Davido",
+                                      style: AppStyles.bodyBold.copyWith(
+                                          color: AppColors.onPrimaryColor)),
+                                  gapHeight(2),
+                                  Text(snapshot.data![index]["name"],
+                                      style: AppStyles.bodyRegularText.copyWith(
+                                          color: AppColors.onPrimaryColor))
+                                ],
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }),
+                gapHeight(20),
+                Padding(
+                  padding: const EdgeInsets.only(left: 15),
+                  child: Text(
+                    "Top Artistes",
+                    style: AppStyles.agTitle3Bold.copyWith(
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                gapHeight(20),
+                const Padding(
+                  padding: EdgeInsets.only(left: 15),
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        TopArtisteWidget(),
+                        TopArtisteWidget(),
+                        TopArtisteWidget(),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       )),
     );
   }
